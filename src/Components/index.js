67b5b0e82261c484f8135a176e3fd291353269a2
mariana-santos <marianasfernandessousa@gmail.com>
2023-07-08 23:59:39 -0300

    ajuste imports

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,18 +1,18 @@
-import NavBar from './Navbar'
-import About from './About'
-import Habilities from './Habilities'
-import Hability from './Hability'
-import SocialMedia from './SocialMedia'
-import Email from './Email'
-// import Experience from './Experience'
-import Experiences from './Experiences'
+// import NavBar from './Navbar'
+// import About from './About'
+// import Habilities from './Habilities'
+// import Hability from './Hability'
+// import SocialMedia from './SocialMedia'
+// import Email from './Email'
+// // import Experience from './Experience'
+// import Experiences from './Experiences'
 
-export {
-    NavBar,
-    About,
-    Habilities,
-    Hability,
-    SocialMedia,
-    Email,
-    Experiences
-}
+// export {
+//     NavBar,
+//     About,
+//     Habilities,
+//     Hability,
+//     SocialMedia,
+//     Email,
+//     Experiences
+// }
