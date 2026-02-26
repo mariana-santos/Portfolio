96ab5e6a620f14d4898c648886a6644f01ef1567
mariana-santos <marianasfernandessousa@gmail.com>
2026-02-26 20:57:59 -0300

    fix: renamed the import

--- src/components/Experiences/index.jsx
+++ src/components/Experiences/index.jsx
@@ -5,7 +5,7 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Experience from "../ExperienceDetails";
+import ExperienceDetails from "../ExperienceDetails";
 
 gsap.registerPlugin(ScrollTrigger);
 
